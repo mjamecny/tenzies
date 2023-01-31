47946e9f0d37d0c05b5e48c145703e3d0040ede8
roydett <94c884c8-c756-4c1a-a30d-d897c94d81b5@anonaddy.me>
2023-01-31 16:39:51 +0100

    styling main interface and Die component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import Die from './components/Die'
 
 const App = () => {
-  return <main>whatever</main>
+  return (
+    <main>
+      <div className="dice-container">
+        <Die value="1" />
+        <Die value="2" />
+        <Die value="3" />
+        <Die value="4" />
+        <Die value="5" />
+        <Die value="6" />
+        <Die value="1" />
+        <Die value="1" />
+        <Die value="1" />
+        <Die value="1" />
+      </div>
+    </main>
+  )
 }
 
 export default App
