475e9ebf4fb7bfdd7b38fd4ac99c4947797e0a4c
roydett <94c884c8-c756-4c1a-a30d-d897c94d81b5@anonaddy.me>
2023-01-31 17:36:31 +0100

    refactor dice state to array of objects and install nanoid package

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import Die from './components/Die'
 import { useState } from 'react'
+import { nanoid } from 'nanoid'
 
 const App = () => {
   // generate array of 10 numbers between 1-6
   const allNewDice = () => {
     let newDice = []
     for (let i = 0; i < 10; i++) {
-      newDice.push(Math.floor(Math.random() * 6) + 1)
+      newDice.push({
+        value: Math.floor(Math.random() * 6) + 1,
+        isHeld: false,
+        id: nanoid(),
+      })
     }
     return newDice
   }
@@ -15,8 +20,8 @@ const App = () => {
   const [dice, setDice] = useState(allNewDice())
 
   // generate dice elements
-  const diceElements = dice.map((die, i) => (
-    <Die key={i} value={die} />
+  const diceElements = dice.map((die) => (
+    <Die key={die.id} value={die.value} />
   ))
 
   const rollDice = () => {
